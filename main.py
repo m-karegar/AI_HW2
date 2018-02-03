dc60eee0fed95d54b0c03813c5099b83f7d67f22
MohammadReza <karegar@gmail.com>
2018-02-03 04:00:49 +0330

    sent version

--- main.py
+++ main.py
@@ -1,18 +1,82 @@
+import numpy as np
 from w2v import Line2Vector
+from sklearn.svm import SVC
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import recall_score
+
+import time
+import random
+
+startTime = time.time()
 
 converter = Line2Vector()
 
 vectorSentences = []
+vectorTestData = []
 
-trainData = open('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/rem_stop_word.txt')
+trainAddress = input('Enter train data address:')
+targetAddress = input('Enter target address:')
+testAddress = input('Enter test data address:')
+testTargetAddress = input('Enter test target address (only for evaluation):')
 
-cnt = 0
-for line in trainData:
+# data = open('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/rem_stop_word.txt').readlines()
+data = open(trainAddress).readlines()
+target = open(testAddress).readlines()
+# target = open('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/train.label').readlines()
+test = open(testAddress).readlines()
+testTarget = open(testTargetAddress).readlines()
+
+for i in range(len(target)):
+    target[i] = int(target[i])
+for i in range(len(testTarget)):
+    testTarget[i] = int(testTarget[i])
+
+
+combined = list(zip(data, target))
+random.shuffle(combined)
+data[:], target[:] = zip(*combined)
+
+# for line in data[:21000]:
+total = min(20000, len(data))
+
+for line in data[:total]:
     vectorSentences.append(converter.get_vector(line))
-    # print(line)
-    # print(vectorSentences[cnt])
-    # cnt += 1
-    # if cnt > 3:
-    #     break
 
-# print(vectorSentences[0])
+for line in test:
+    vectorTestData.append(converter.get_vector(line))
+
+# print("w2v: " + str(time.time() - startTime))
+startTime = time.time()
+
+trainData = vectorSentences[:total]
+trainTarget = target[:total]
+
+# testData = vectorSentences[0.9 * total:total]
+# testTarget = target[0.9 * total:total]
+testData = vectorTestData
+
+
+clf = SVC(kernel='linear', C=1)
+# scores = cross_val_score(clf, trainData, trainTarget, cv=5)
+
+scoring = ['accuracy', 'precision_macro', 'recall_macro']
+scores = cross_validate(clf, trainData, trainTarget, scoring=scoring, cv=5, return_train_score=False)
+
+
+for scr in scores:
+    print(scr + ' ' + str(scores[scr]))
+
+clf2 = SVC().fit(trainData, trainTarget)
+
+predicted = clf2.predict(testData)
+
+print(metrics.classification_report(testTarget, predicted))
+
+f = open('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/predicted.txt', 'w')
+for item in predicted:
+    f.write(str(item) + '\n')
+f.close()
+
+# print("the rest: " + str(time.time() - startTime))
--- w2v.py
+++ w2v.py
@@ -1,23 +1,46 @@
 import gensim
-
-# model = gensim.models.Word2Vec.load('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/FinalModel')#, encoding = 'utf-8')
-# model = gensim.models.Word2Vec.load_word2vec_format('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/FinalModel')
-# model = gensim.models.KeyedVectors.load_word2vec_format('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/FinalModel')
-
-# print(model['مرد'])
-# print(model['زن'])
-# print(model['سوکابلیت'])
-# print(model.similarity('دست', 'پا'))
-# print(model.similarity('شتر', 'چین'))
-# # print(model.doesnt_match('درخت', 'بشقاب', 'شاخه', 'برگ'))
-# print(model.doesnt_match("دست پا موز گوش".split()))
+import math
+import time
+import operator
 
 
 class Line2Vector:
     def __init__(self):
         self.w2v = gensim.models.KeyedVectors.load_word2vec_format(
             '/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/FinalModel')
+        """self.appearance_in_lines = {}
+        tmp = time.time()
+        self.lines = open('/Users/MohammadReza/Desktop/Uni/Programs/AI_HW2/rem_stop_word.txt').readlines(100000)
+        print(len(self.lines))
+        self.unique_lines_count()
+        print(time.time() - tmp)
 
+    def unique_lines_count(self):
+        for line in self.lines:
+            for word in line.split():
+                if word not in self.appearance_in_lines:
+                    self.appearance_in_lines[word] = 0
+                    for l in self.lines:
+                        if word in l.split():
+                            self.appearance_in_lines[word] += 1
+
+        tmp = sorted(self.appearance_in_lines.items(), key=lambda x:x[1], reverse=True)
+        cnt = 0
+        for w in tmp:
+            print(w)
+            cnt += 1
+            if cnt > 20:
+                break
+
+    def idf(self, word):
+        return float(math.log(len(self.lines)) / (1 + self.appearance_in_lines[word]))
+
+    def tf(self, word, line):
+        return float(line.split.count(word) / len(line.split))
+
+    def tfidf(self, word, line):
+        return self.tf(word, line) * self.idf(word)
+    """
     def get_vector(self, line):
         cnt = 0
         res = [0 for i in range(100)]
@@ -25,11 +48,13 @@ class Line2Vector:
             try:
                 tmp = self.w2v[word]
                 res += tmp
+                cnt += 1
             except KeyError:
                 pass
 
         if cnt != 0:
-            res /= cnt
+            for i in range(len(res)):
+                res[i] = float(res[i] / cnt)
 
         return res
 
